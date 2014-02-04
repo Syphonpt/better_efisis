e53680baee4a9e19c03b9d161698d20e415e6e88
Joao Santos <norwat@krutt.org>
2014-02-04 16:28:28 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'bcrypt-ruby', '~> 3.1.2'
+
+gem 'sidekiq', '2.17.4'
+gem 'sidetiq', '0.5.0'
+
+gem 'mechanize' 
+gem 'rest-client'
+
+gem 'bootstrap-sass'
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Better::Application.config.secret_key_base = 'deef0d1d96f44121f51233cb382f0310ffda710f5c0e95853cf83ae5ba86677a5730815c85abfb249a603e390a51101416c888e47fb2b5ac3265eeda5a841205'
