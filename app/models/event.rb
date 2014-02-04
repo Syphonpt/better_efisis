2c528ae6df2e04413824d1cadc77e09190657132
Joao Santos <norwat@krutt.org>
2014-02-04 16:57:59 +0000

    Criadas restrições ao modelo event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,13 @@
 class Event < ActiveRecord::Base
+	 has_many :odds
+	 has_many :runners
+
+	 validates :id,					 uniqueness: true
+	 validates :market_id,	 presence: true
+	 validates :home,				 presence: true
+	 validates :away,				 presence: true
+	 validates :cc,					 presence: true
+	 validates :date,				 presence: true
+
+	 before_save { self.cc   = cc.downcase } 
 end
